4f8e57f1a945c0a566732f602b4800cdc70bc425
Kaveh <52333921+0xkaveh@users.noreply.github.com>
2024-10-20 16:49:28 +0100

    Update Network-Monitor.py

--- Network-Monitor.py
+++ Network-Monitor.py
@@ -1,3 +1,31 @@
+"""
+
+  ____    __     __    __   ___     ____     __    __    _____   __    __  
+ / __ \  (_ \   / _)  () ) / __)   (    )    ) )  ( (   / ___/  (  \  /  ) 
+( (  ) )   \ \_/ /    ( (_/ /      / /\ \   ( (    ) ) ( (__     \ (__) /  
+( (  ) )    \   /     ()   (      ( (__) )   \ \  / /   ) __)     ) __ (   
+( (  ) )    / _ \     () /\ \      )    (     \ \/ /   ( (       ( (  ) )  
+( (__) )  _/ / \ \_   ( (  \ \    /  /\  \     \  /     \ \___    ) )( (   
+ \____/  (__/   \__)  ()_)  \_\  /__(  )__\     \/       \____\  /_/  \_\  
+
+
+     Network-Monitor.py
+---------------------------------
+    Author: 0xKaveh (https://x.com/0xKaveh)
+    A simple app to monitor 
+    download and upload speeds
+---------------------------------
+fell free to make it better 
+if you want to make to .exe just do this command respectively 
+1. pip install pyinstaller
+2. cd path\to\your\script
+3. pyinstaller --onefile Network-Monitor.py
+4. (if you want to make it windowed not console) pyinstaller --onefile --windowed Network-Monitor.py 
+5. go to /dist/Network-Monitor.exe 
+6. run it
+"""
+
+
 import sys
 import psutil
 from PyQt5.QtCore import Qt, QTimer
